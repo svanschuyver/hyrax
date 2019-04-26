c5f73a211c547cf49020f77b854a72c9c4aba1eb
Daniel Pierce <dlpierce@indiana.edu>
2019-04-26 17:12:24 -0400

    Index versioned file URI for IIIF, improve thumbnail cache behavior. Fetch content version uri from Fedora if not indexed.

--- app/presenters/hyrax/displays_image.rb
+++ app/presenters/hyrax/displays_image.rb
@@ -11,11 +11,13 @@ module Hyrax
     # @return [IIIFManifest::DisplayImage] the display image required by the manifest builder.
     def display_image
       return nil unless ::FileSet.exists?(id) && solr_document.image? && current_ability.can?(:read, id)
-      # @todo this is slow, find a better way (perhaps index iiif url):
-      original_file = ::FileSet.find(id).original_file
+
+      latest_file_id = current_file_version || unindexed_current_file_version
+
+      return nil unless latest_file_id
 
       url = Hyrax.config.iiif_image_url_builder.call(
-        original_file.id,
+        latest_file_id,
         request.base_url,
         Hyrax.config.iiif_image_size_default
       )
@@ -23,7 +25,7 @@ module Hyrax
       IIIFManifest::DisplayImage.new(url,
                                      width: 640,
                                      height: 480,
-                                     iiif_endpoint: iiif_endpoint(original_file.id))
+                                     iiif_endpoint: iiif_endpoint(latest_file_id))
     end
 
     private
@@ -34,6 +36,11 @@ module Hyrax
           Hyrax.config.iiif_info_url_builder.call(file_id, request.base_url),
           profile: Hyrax.config.iiif_image_compliance_level_uri
         )
+      end
+
+      def unindexed_current_file_version
+        Rails.logger.warn "Indexed current_file_version for #{id} not found, falling back to Fedora."
+        ActiveFedora::File.uri_to_id(::FileSet.find(id).current_content_version_uri)
       end
   end
 end
